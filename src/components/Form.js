449c751eb25b24b94b6da3e68f129c998c01aea7
Shourav Mandal <shourav.mandal55@myhunter.cuny.edu>
2023-05-02 18:57:44 -0400

    initial commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,74 @@
-import { useState } from 'react'
+import React, {Component} from 'react';
 
-const Form = () => {
-  const handleChange = (event) => {
-    /*
-            TODO - Logic for changing state based on form changes
-        */
-  }
+class Form extends Component {
 
-  const onFormSubmit = (event) => {
-    // to prevent page reload on form submit
-    event.preventDefault()
+    constructor(props) {
 
-    /*
-            TODO - Logic for calling props to handle submission and setting state changes
-        */
-  }
+    super(props);
 
-  return (
-    <form>
-      {/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
-    </form>
-  )
+/* TODO - set initial state for link name and URL */
+
+        this.state = {
+
+        linkName: '',
+
+        url: ''
+
 }
 
-export default Form
+}
+
+        handleChange = event => {
+
+/* TODO - Logic for changing state based on form changes */
+
+        if(event.target.id === "link")
+
+        this.setState({ linkName: event.target.value});
+      else
+
+      this.setState({ url: event.target.value});
+
+    }
+
+      onFormSubmit = event => { 
+
+      event.preventDefault();
+
+/* TODO - Logic for calling props to handle submission and setting state changes */
+
+      let newFavLink = {linkName: this.state.linkName, url: this.state.url};
+
+        this.props.addNew(newFavLink); // addNew was passed as props from LinkContainer
+
+        this.setState({ linkName: '', url: ''}); // reset the states
+
+      }
+
+      render() {
+
+  return(
+
+<form>
+
+{/* TODO - Logic for returning a form element with labels and inputs for link name and URL */}
+
+            <label htmlFor="link">Link Name : </label>
+
+            <input type="text" id="link" value={this.state.linkName} onChange={(event) =>this.handleChange(event)}/>
+
+            <label htmlFor="url">URL : </label>
+
+            <input type="text" id="url" value={this.state.url} onChange={(event) =>this.handleChange(event)}/>
+            
+            <button type="submit" onClick={(e)=>this.onFormSubmit(e)}>Submit</button>
+
+      </form>
+
+      )
+
+    }
+
+  }
+
+export default Form;
--- src/components/LinkContainer.js
+++ src/components/LinkContainer.js
@@ -1,32 +1,73 @@
-import { useState } from 'react'
-// import Table from './Table';
-// import Form from './Form';
+import React from 'react';
 
-const LinkContainer = (props) => {
-  const handleRemove = (index) => {
-    /*
-            TODO - Create logic for setting the state to filter array and remove favLink at index
-        */
+import Table from './Table';
+
+import Form from './Form';
+
+class LinkContainer extends React.Component {
+
+  constructor(props){
+
+  super(props)
+
+/* TODO - Create state object for storing favLinks */
+
+      this.state = {favLinks: [],}
+
   }
 
-  const handleSubmit = (favLink) => {
-    /*
-            TODO - Create logic to set state and add new favLink to favLinks array in state
-        */
-  }
+    removeCharacter = index => {
 
-  return (
-    <div className="container">
-      <h1>My Favorite Links</h1>
-      <p>Add a new url with a name and link to the table.</p>
-      {/*TODO - Add Table Component */}
+/* TODO - Create logic for setting the state to filter array and remove favLink at index */
 
-      <br />
+        const tempArray = this.state.favLinks;
 
-      <h3>Add New</h3>
-      {/*TODO - Add Form Component */}
-    </div>
-  )
+        tempArray.splice(index, 1);
+
+        this.setState ({ favLinks : tempArray });
+
 }
 
-export default LinkContainer
+handleSubmit = favLink => {
+
+/* TODO - Create logic to setState and add new favLink to favLinks array in state */
+
+    this.setState({
+
+      favLinks: [...this.state.favLinks, favLink]
+
+    });
+
+  }
+
+  render() {
+
+  return (
+
+      <div className="container">
+
+          <h1>My Favorite Links</h1>
+
+          <p>Add a new url with a name and link to the table.</p>
+
+      {/*TODO - Add Table Component */}
+
+        <Table linkData= {this.state.favLinks} removeLink= {(index) => this.removeCharacter(index)} />
+
+    <br/>
+
+          <h3>Add New</h3>
+
+      {/*TODO - Add Form Component */}
+
+    <Form addNew= {(favLink) => this.handleSubmit(favLink)} />
+
+  </div>
+
+  );
+
+}
+
+}
+
+export default LinkContainer;
